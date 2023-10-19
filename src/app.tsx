bae09b32032dfba08a8e9a2697500937b9b44ddd
sahara.ap <sahara.ap13@gmail.com>
2023-10-19 21:10:44 +0300

    Подключение HelmetProvider

--- src/app.tsx
+++ src/app.tsx
@@ -6,6 +6,7 @@ import FavoritePage from './pages/favorites-page/favorite-page';
 import OfferPage from './pages/offer-page/offer-page';
 import NotFound from './pages/404-page/404-page';
 import PrivateRoute from './components/private-route/private-route';
+import { HelmetProvider } from 'react-helmet-async';
 
 type AppProps = {
   offersCount: number;
@@ -13,24 +14,44 @@ type AppProps = {
 
 function App({ offersCount }: AppProps): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={'/'} element={<MainPage offersCount={offersCount} />} />
-        <Route path={'login'} element={<LoginPage />} />
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={'/'}
+            element={<MainPage offersCount={offersCount} />}
+          />
+          <Route
+            path={'login'}
+            element={<LoginPage />}
+          />
 
-        <Route path={'favorites'} element={
-          <PrivateRoute>
-            <FavoritePage />
-          </PrivateRoute>
-        }
-        />
-        <Route path={'offer'}>
-          <Route index element={<OfferPage />} />
-          <Route path={':id'} element={<OfferPage />} />
-        </Route>
-        <Route path={'*'} element={<NotFound />} />
-      </Routes>
-    </BrowserRouter >
+          <Route
+            path={'favorites'}
+            element={
+              <PrivateRoute>
+                <FavoritePage />
+              </PrivateRoute>
+            }
+          />
+          <Route path={'offer'}>
+            <Route
+              index
+              element={<OfferPage />}
+            />
+            <Route
+              path={':id'}
+              element={<OfferPage />}
+            />
+          </Route>
+          <Route
+            path={'*'}
+            element={<NotFound />}
+          />
+        </Routes>
+      </BrowserRouter >
+    </HelmetProvider>
+
   );
 }
 
