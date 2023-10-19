776603e20c1c222626b258b6a539568c02d15eeb
sahara.ap <sahara.ap13@gmail.com>
2023-10-19 16:30:01 +0300

    WIP Реализована маршрутизация ко всем страницам приложения

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import MainPage from './pages/main-page/mainPage';
+import LoginPage from './pages/login-page/loginPage';
+import FavoritePage from './pages/favorites-page/favoritePage';
+import OfferPage from './pages/offer-page/offerPage';
+import NotFound from './pages/404-page/404Page';
 
 type AppProps = {
   offersCount: number;
 }
 
-function App({offersCount}: AppProps): JSX.Element {
+function App({ offersCount }: AppProps): JSX.Element {
   return (
-    <MainPage offersCount={offersCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<MainPage offersCount={offersCount} />} />
+        <Route path={'login'} element={<LoginPage />} />
+        <Route path={'favorites'} element={<FavoritePage />} />
+        <Route path={'offer'} element={<OfferPage />} />
+        <Route path={'*'} element={<NotFound />} />
+      </Routes>
+    </BrowserRouter >
   );
 }
 
