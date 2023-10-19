0582b0d5b6f8d6d5abf2164e05eff83621091abc
sahara.ap <sahara.ap13@gmail.com>
2023-10-19 16:26:41 +0300

    Создан компонент 404Page

---
+++ src/pages/404-page/404Page.tsx
@@ -0,0 +1,11 @@
+import './404Page.css';
+
+function NotFound() {
+  return (
+    <div className="notfound">
+      <h1 className="notfound--title">404 NOT FOUND</h1>
+    </div>
+  );
+}
+
+export default NotFound;
