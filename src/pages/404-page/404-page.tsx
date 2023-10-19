d4d980a7319a72939108996210f9e710fdb37f18
sahara.ap <sahara.ap13@gmail.com>
2023-10-19 21:12:03 +0300

    Изменение логики стилизации 404page с помощью плагина css-modules

--- src/pages/404-page/404-page.tsx
+++ src/pages/404-page/404-page.tsx
@@ -1,15 +1,19 @@
+import styles from './404-page.module.css';
+
 import { Link } from 'react-router-dom';
-import './404-page.css';
+import { Helmet } from 'react-helmet-async';
 
 function NotFound() {
   return (
-
-    <div className="notfound">
-      <h1 className="notfound--title">404 NOT FOUND</h1>
+    <div className={`page ${styles.notfound}`}>
+      <Helmet>
+        <title>{'6 cities - Not Found'}</title>
+      </Helmet>
+      <h1 className={styles.title}>404 NOT FOUND</h1>
       <h3>
-        <Link to={'/'} className="link">Go to main page</Link>
+        <Link to={'/'}>Go to main page</Link>
       </h3>
-    </div>
+    </div >
 
   );
 }
