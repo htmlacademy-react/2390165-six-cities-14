3f57bdb3ecbf7289e5d0c7b43d93d77a8ae343cf
sahara.ap <sahara.ap13@gmail.com>
2023-10-19 17:47:10 +0300

    WIP Создан компонент для реализации приватных маршрутов

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,7 @@ import LoginPage from './pages/login-page/login-page';
 import FavoritePage from './pages/favorites-page/favorite-page';
 import OfferPage from './pages/offer-page/offer-page';
 import NotFound from './pages/404-page/404-page';
+import PrivateRoute from './components/private-route/private-route';
 
 type AppProps = {
   offersCount: number;
@@ -16,7 +17,13 @@ function App({ offersCount }: AppProps): JSX.Element {
       <Routes>
         <Route path={'/'} element={<MainPage offersCount={offersCount} />} />
         <Route path={'login'} element={<LoginPage />} />
-        <Route path={'favorites'} element={<FavoritePage />} />
+
+        <Route path={'favorites'} element={
+          <PrivateRoute>
+            <FavoritePage />
+          </PrivateRoute>
+        }
+        />
         <Route path={'offer'}>
           <Route index element={<OfferPage />} />
           <Route path={':id'} element={<OfferPage />} />
