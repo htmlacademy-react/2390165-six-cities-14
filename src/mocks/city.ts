b61425826edefdb04b260f7a1dcb034d4f0bb5bb
sahara.ap <sahara.ap13@gmail.com>
2023-11-03 08:56:12 +0300

    Изменены моковые данные для отображения их на карте

---
+++ src/mocks/city.ts
@@ -0,0 +1,15 @@
+type City = {
+  title: string;
+  lat: number;
+  lng: number;
+  zoom: number;
+}
+
+const city: City = {
+  title: 'Amsterdam',
+  lat: 52.37454,
+  lng: 4.897976,
+  zoom: 10,
+};
+
+export {city};
