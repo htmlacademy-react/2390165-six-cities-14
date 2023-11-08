594b947f4a818e43a10cf9cefb56aa3d538fd99f
sahara.ap <sahara.ap13@gmail.com>
2023-11-08 13:49:34 +0300

    Реализовано состояние выпадающего списка сортировки

--- src/components/sort/sort.tsx
+++ src/components/sort/sort.tsx
@@ -2,36 +2,50 @@ import { useState } from 'react';
 
 function Sort(): JSX.Element {
   const values = ['Popular', 'Price: low to high', 'Price: high to low', 'Top rated first'];
-  const items = values.map((value, ind) => ({
+  const items = values.map((value) => ({
     value,
-    isSelected: ind === 0
+    isSelected: false
   }));
 
   const [state, setState] = useState(items);
+  const [ulClassName, setUlClassName] = useState('places__options--closed');
 
-  function handleClick(event: React.MouseEvent<HTMLLIElement> & { target: HTMLLIElement }) {
+  function handleUlClick() {
+    setUlClassName('places__options--opened');
+  }
+
+  function handleLiClick(event: React.MouseEvent<HTMLLIElement> & { target: HTMLLIElement }) {
     const selectedElement = event.target;
     items.forEach((it) => (it.isSelected = selectedElement.textContent === it.value));
 
+    setUlClassName('places__options--closed');
     setState(items);
   }
+
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
-        {state.find((item) => item.isSelected)?.value}
+      <span
+        className="places__sorting-type"
+        tabIndex={0}
+        onClick={handleUlClick}
+      >
+        {state.find((item) => item.isSelected)?.value || 'Popular'}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
+      <ul
+        className={`places__options places__options--custom ${ulClassName}`}
+
+      >
         {
           state.map((item) => (
             <li
               key={item.value}
               className={`places__option ${item.isSelected ? 'places__option--active' : ''}`}
               tabIndex={0}
-              onClick={handleClick}
+              onClick={handleLiClick}
             >
               {item.value}
             </li>
