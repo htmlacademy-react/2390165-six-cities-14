d54f0a1584899ccb5d248ba4be57477a29762a40
sahara.ap <sahara.ap13@gmail.com>
2023-11-16 15:47:58 +0300

    Реализовано присвоение класса active для выбранного фильтра

--- src/components/filter/filter.tsx
+++ src/components/filter/filter.tsx
@@ -1,10 +1,13 @@
-import { useAppDispatch } from '../../hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 import { setCity } from '../../store/actions';
 
 import Filters from '../../types/filters';
 import { ActiveCity } from '../../types/city';
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
 
 function Filter(): JSX.Element {
+  const selectedCity = useAppSelector((state) => state.activeCity);
   const filters: Filters = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'];
   const dispatch = useAppDispatch();
 
@@ -20,9 +23,12 @@ function Filter(): JSX.Element {
           className="locations__item"
           onClick={handleClick(filter)}
         >
-          <a className="locations__item-link tabs__item" href="#">
+          <Link
+            to={AppRoute.Main}
+            className={`${filter === selectedCity ? 'tabs__item--active ' : ''}locations__item-link tabs__item`}
+          >
             <span>{filter}</span>
-          </a>
+          </Link>
         </li>
       ))}
 
