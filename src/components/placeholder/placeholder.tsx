acc45af3f3f3a847df3ffae0dccd1974e72c5049
sahara.ap <sahara.ap13@gmail.com>
2023-11-26 11:21:04 +0300

    fix. Изменена логика отрисовки Placeholder

--- src/components/placeholder/placeholder.tsx
+++ src/components/placeholder/placeholder.tsx
@@ -1,10 +1,8 @@
 import styles from './placeholder.module.css';
 
 import { useEffect } from 'react';
-import { useAppSelector } from '../../hooks';
 
 function PlaceHolder(): JSX.Element | null {
-  const isReady = useAppSelector((state) => state.isLoaded);
   const message = 'Loading...';
 
   function handleDocumentClick(event: MouseEvent) {
@@ -15,10 +13,6 @@ function PlaceHolder(): JSX.Element | null {
     document.addEventListener('click', handleDocumentClick);
     return(() => document.removeEventListener('click', handleDocumentClick));
   }, []);
-
-  if (isReady) {
-    return null;
-  }
 
   return (
     <p className={`${styles.message}`}>{message}</p>
