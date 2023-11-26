3dfea781f9e579866c7aed4211eab29c952602f4
sahara.ap <sahara.ap13@gmail.com>
2023-11-26 11:18:43 +0300

    Добавлены actions для получения данных с сервера

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { createAction } from '@reduxjs/toolkit';
 import { ActiveCity } from '../types/city';
+import { Favs, NearOffer, Offer, SelectedOffer } from '../types/offer';
+import ReviewType from '../types/review';
 
 const setCity = createAction<{ city: ActiveCity }>('setCity');
 
@@ -11,10 +13,33 @@ const setOffers = createAction('setOffers', (value) => (
     }
   }
 ));
+const favoritesNumber = createAction<number>('favoritesNumber');
+
+const fetchOffers = createAction<Offer[]>('fetchOffers');
 const isLoaded = createAction('loaded');
+
+const fetchSelectedOffer = createAction<SelectedOffer>('fetchOffer');
+const isSelectedOfferLoaded = createAction('selectedOfferLoaded');
+const fetchFavs = createAction<Favs[]>('fetchFavs');
+const isFavsLoaded = createAction('favsLoaded');
+const fetchNearPlaces = createAction<NearOffer[]>('fetchNearPlaces');
+const isNearPlacesLoaded = createAction('nearPlacesLoaded');
+const fetchReviews = createAction<ReviewType[]>('fetchReviews');
+const isReviewsLoaded = createAction('reviewsLoaded');
+
 
 export {
   setCity,
   setOffers,
-  isLoaded
+  favoritesNumber,
+  fetchOffers,
+  isLoaded,
+  fetchSelectedOffer,
+  isSelectedOfferLoaded,
+  fetchFavs,
+  isFavsLoaded,
+  fetchNearPlaces,
+  isNearPlacesLoaded,
+  fetchReviews,
+  isReviewsLoaded
 };
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,33 +1,82 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import {createReducer} from '@reduxjs/toolkit';
+import { createReducer } from '@reduxjs/toolkit';
 
 // import offers from '../mocks/offers';
-import { isLoaded, setCity, setOffers } from './actions';
+import { fetchFavs, fetchNearPlaces, fetchSelectedOffer, fetchOffers, fetchReviews, isLoaded, setCity, setOffers, isSelectedOfferLoaded, isFavsLoaded, isNearPlacesLoaded, isReviewsLoaded, favoritesNumber } from './actions';
 import { ActiveCity } from '../types/city';
-import { Offer } from '../types/offer';
+import { Favs, NearOffer, Offer, SelectedOffer } from '../types/offer';
+import ReviewType from '../types/review';
 
 type InitialState = {
   activeCity: ActiveCity;
+  favoritesNumber: number;
   offers: Array<Offer>;
   isLoaded: boolean;
+  selectedOffer: SelectedOffer | null;
+  isSelectedOfferLoaded: boolean;
+  favs: Favs[];
+  isFavsLoaded: boolean;
+  nearPlaces: NearOffer[];
+  isNearPlacesLoaded: boolean;
+  reviews: ReviewType[];
+  isReviewsLoaded: boolean;
 }
 const initialState: InitialState = {
   activeCity: 'Paris',
+  favoritesNumber: 0,
   offers: [],
   isLoaded: false,
+  selectedOffer: null,
+  isSelectedOfferLoaded: false,
+  favs: [],
+  isFavsLoaded: false,
+  nearPlaces: [],
+  isNearPlacesLoaded: false,
+  reviews: [],
+  isReviewsLoaded: false,
 };
 
 const reducer = createReducer(initialState, (builder) => {
   builder
     .addCase(setCity, (state, action) => {
-      const {city} = action.payload;
+      const { city } = action.payload;
       state.activeCity = city;
+    })
+    .addCase(favoritesNumber, (state, action) => {
+      state.favoritesNumber = state.favoritesNumber + action.payload;
     })
     .addCase(setOffers, (state, action) => {
       state.offers = action.payload.offers;
     })
     .addCase(isLoaded, (state) => {
       state.isLoaded = true;
+    })
+    .addCase(fetchOffers, (state, action) => {
+      state.offers = action.payload;
+    })
+    .addCase(fetchSelectedOffer, (state, action) => {
+      state.selectedOffer = action.payload;
+    })
+    .addCase(isSelectedOfferLoaded, (state) => {
+      state.isSelectedOfferLoaded = true;
+    })
+    .addCase(fetchFavs, (state, action) => {
+      state.favs = action.payload;
+    })
+    .addCase(isFavsLoaded, (state) => {
+      state.isFavsLoaded = true;
+    })
+    .addCase(fetchNearPlaces, (state, action) => {
+      state.nearPlaces = action.payload;
+    })
+    .addCase(isNearPlacesLoaded, (state) => {
+      state.isNearPlacesLoaded = true;
+    })
+    .addCase(fetchReviews, (state, action) => {
+      state.reviews = action.payload;
+    })
+    .addCase(isReviewsLoaded, (state) => {
+      state.isReviewsLoaded = true;
     });
 });
 
