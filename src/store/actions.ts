2837fa504756455814638d7ccf0acd8dd283e8ed
sahara.ap <sahara.ap13@gmail.com>
2023-11-27 18:31:07 +0300

    Добавлен action setError

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -6,17 +6,21 @@ import { AuthStatus } from '../const';
 
 const setCity = createAction<{ city: ActiveCity }>('setCity');
 
-const setOffers = createAction<Offer[]>('setOffers');
 const favoritesNumber = createAction<number>('favoritesNumber');
 
+const setOffers = createAction<Offer[]>('setOffers');
 const isLoaded = createAction<boolean>('loaded');
+const setError = createAction<string | null>('setError');
 
 const setSelectedOffer = createAction<SelectedOffer>('data/setSelectedOffer');
 const isSelectedOfferLoaded = createAction('selectedOfferLoaded');
+
 const setFavs = createAction<Favs[]>('data/setFavs');
 const isFavsLoaded = createAction('favsLoaded');
+
 const setNearPlaces = createAction<Offer[]>('data/setNearPlaces');
 const isNearPlacesLoaded = createAction('nearPlacesLoaded');
+
 const setReviews = createAction<ReviewType[]>('data/setReviews');
 const isReviewsLoaded = createAction('reviewsLoaded');
 
@@ -25,16 +29,23 @@ const requireAuthorization = createAction<AuthStatus>('user/requireAuthorization
 
 export {
   setCity,
-  setOffers,
   favoritesNumber,
+
+  setOffers,
   isLoaded,
+  setError,
+
   setSelectedOffer,
   isSelectedOfferLoaded,
+
   setFavs,
   isFavsLoaded,
+
   setNearPlaces,
   isNearPlacesLoaded,
+
   setReviews,
   isReviewsLoaded,
+
   requireAuthorization,
 };
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { createReducer } from '@reduxjs/toolkit';
 
-import { setFavs, setNearPlaces, setSelectedOffer, setReviews, isLoaded, setCity, setOffers, isSelectedOfferLoaded, isFavsLoaded, isNearPlacesLoaded, isReviewsLoaded, favoritesNumber, requireAuthorization } from './actions';
+import { setFavs, setNearPlaces, setSelectedOffer, setReviews, isLoaded, setCity, setOffers, isSelectedOfferLoaded, isFavsLoaded, isNearPlacesLoaded, isReviewsLoaded, favoritesNumber, requireAuthorization, setError } from './actions';
 import { ActiveCity } from '../types/city';
 import { Favs, Offer, SelectedOffer } from '../types/offer';
 import ReviewType from '../types/review';
@@ -10,31 +10,45 @@ import { AuthStatus } from '../const';
 type InitialState = {
   activeCity: ActiveCity;
   favoritesNumber: number;
+
   offers: Array<Offer>;
   isLoaded: boolean;
+  error: string | null;
+
   selectedOffer: SelectedOffer | null;
   isSelectedOfferLoaded: boolean;
+
   favs: Favs[];
   isFavsLoaded: boolean;
+
   nearPlaces: Offer[];
   isNearPlacesLoaded: boolean;
+
   reviews: ReviewType[];
   isReviewsLoaded: boolean;
+
   authStatus: AuthStatus;
 }
 const initialState: InitialState = {
   activeCity: 'Paris',
   favoritesNumber: 0,
+
   offers: [],
   isLoaded: false,
+  error: null,
+
   selectedOffer: null,
   isSelectedOfferLoaded: false,
+
   favs: [],
   isFavsLoaded: false,
+
   nearPlaces: [],
   isNearPlacesLoaded: false,
+
   reviews: [],
   isReviewsLoaded: false,
+
   authStatus: AuthStatus.Unknown,
 };
 
@@ -47,36 +61,45 @@ const reducer = createReducer(initialState, (builder) => {
     .addCase(favoritesNumber, (state, action) => {
       state.favoritesNumber = state.favoritesNumber + action.payload;
     })
+
     .addCase(setOffers, (state, action) => {
       state.offers = action.payload;
     })
     .addCase(isLoaded, (state, action) => {
       state.isLoaded = action.payload;
     })
+    .addCase(setError, (state, action) => {
+      state.error = action.payload;
+    })
+
     .addCase(setSelectedOffer, (state, action) => {
       state.selectedOffer = action.payload;
     })
     .addCase(isSelectedOfferLoaded, (state) => {
       state.isSelectedOfferLoaded = true;
     })
+
     .addCase(setFavs, (state, action) => {
       state.favs = action.payload;
     })
     .addCase(isFavsLoaded, (state) => {
       state.isFavsLoaded = true;
     })
+
     .addCase(setNearPlaces, (state, action) => {
       state.nearPlaces = action.payload;
     })
     .addCase(isNearPlacesLoaded, (state) => {
       state.isNearPlacesLoaded = true;
     })
+
     .addCase(setReviews, (state, action) => {
       state.reviews = action.payload;
     })
     .addCase(isReviewsLoaded, (state) => {
       state.isReviewsLoaded = true;
     })
+
     .addCase(requireAuthorization, (state, action) => {
       state.authStatus = action.payload;
     });
