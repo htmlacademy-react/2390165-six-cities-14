3f855aaeb51521cf5dfdba51d283a5bc027c415f
sahara.ap <sahara.ap13@gmail.com>
2023-12-02 21:51:08 +0300

    Реализован rootReducer

---
+++ src/store/root-reducer.ts
@@ -0,0 +1,14 @@
+import { combineReducers } from '@reduxjs/toolkit';
+import { NameSpace } from '../const';
+import offersData from './offer-data/offer-data';
+import appProcess from './app-process/app-process';
+import userProcess from './users-process/user-process';
+
+const rootReducer = combineReducers({
+  [NameSpace.Data]: offersData.reducer,
+  [NameSpace.App]: appProcess.reducer,
+  [NameSpace.User]: userProcess.reducer
+});
+
+export default rootReducer;
+
