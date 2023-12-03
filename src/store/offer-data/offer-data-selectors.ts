d0f3ad8198e269e52f3b4b979f70fafb9b48ad3e
sahara.ap <sahara.ap13@gmail.com>
2023-12-03 11:00:36 +0300

    Добавлены селекторы

---
+++ src/store/offer-data/offer-data-selectors.ts
@@ -0,0 +1,25 @@
+import { NameSpace } from '../../const';
+import { Favorite, Offer, SelectedOffer } from '../../types/offer';
+import ReviewType from '../../types/review';
+import { State } from '../../types/state';
+
+const getOffers = (state: State): Offer[] => state[NameSpace.Data].offers;
+const getSelectedOffer = (state: State): SelectedOffer | null => state[NameSpace.Data].selectedOffer;
+const getNearPlaces = (state: State): Offer[] => state[NameSpace.Data].nearPlaces;
+const getReviews = (state: State): ReviewType[] => state[NameSpace.Data].reviews;
+const getFavs = (state: State): Favorite[] => state[NameSpace.Data].favs;
+
+const getIsReviewSending = (state: State): boolean => state[NameSpace.Data].isReviewSending;
+const getIsLoaded = (state: State): boolean => state[NameSpace.Data].isLoaded;
+
+export {
+  getOffers,
+  getSelectedOffer,
+  getNearPlaces,
+  getReviews,
+  getFavs,
+
+
+  getIsReviewSending,
+  getIsLoaded,
+};
