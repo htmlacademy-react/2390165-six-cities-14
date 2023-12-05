da1f6a476960ac0015ed1c0ff2dd2afc5c84b154
sahara.ap <sahara.ap13@gmail.com>
2023-12-05 17:49:33 +0300

    Исправлены все ошибки тестов Pages

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,9 @@
 import { Link, useLocation } from 'react-router-dom';
 
-import { AppRoute } from '../../const';
+import { AppRoute, AuthStatus } from '../../const';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { logoutAction } from '../../store/api-actions';
-import { getUserData } from '../../store/users-process/user-process-selectors';
+import { getAuthStatus, getUserData } from '../../store/users-process/user-process-selectors';
 import { getFavs } from '../../store/offer-data/offer-data-selectors';
 
 function Header(): JSX.Element {
@@ -12,6 +12,7 @@ function Header(): JSX.Element {
 
   const favs = useAppSelector(getFavs);
   const userData = useAppSelector(getUserData);
+  const authStatus = useAppSelector(getAuthStatus);
 
   const isMain = pathname === AppRoute.Main as string;
   const isLogin = pathname === AppRoute.Login as string;
@@ -37,37 +38,52 @@ function Header(): JSX.Element {
             </Link>
           </div>
           {
-            !isLogin &&
+            !isLogin && authStatus === AuthStatus.Auth &&
             <nav className="header__nav">
               <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <Link className="header__nav-link header__nav-link--profile" to={userData ? AppRoute.Favorite : AppRoute.Login}>
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name header__login">{userData ? userData.email : 'Sign in'}</span>
-                    {
-                      userData &&
-                      <span className="header__favorite-count">{favs.length}</span>
-                    }
-                  </Link>
-                </li>
                 {
                   userData &&
-                  <li className="header__nav-item">
-                    <Link
-                      className="header__nav-link"
-                      to="/"
-                      onClick={handleSignOutClick}
-                    >
-                      <span
-                        className="header__signout"
+                  <>
+                    <li className="header__nav-item user">
+                      <Link className="header__nav-link header__nav-link--profile" to={userData ? AppRoute.Favorite : AppRoute.Login}>
+                        <div style={{ backgroundImage: `url(${userData.avatarUrl})`, borderRadius: '50%' }} className="header__avatar-wrapper user__avatar-wrapper">
+                        </div>
+                        <span className="header__user-name user__name header__login">{userData ? userData.email : 'Sign in'}</span>
+
+                        <span className="header__favorite-count">{favs.length}</span>
+
+                      </Link>
+                    </li>
+
+                    <li className="header__nav-item">
+                      <Link
+                        className="header__nav-link"
+                        to="/"
+                        onClick={handleSignOutClick}
                       >
-                        Sign out
-                      </span>
-                    </Link>
-                  </li>
+                        <span
+                          className="header__signout"
+                        >
+                          Sign out
+                        </span>
+                      </Link>
+                    </li>
+                  </>
                 }
 
+              </ul>
+            </nav>
+          }
+          {
+            authStatus === AuthStatus.NoAuth &&
+            <nav className='header__nav'>
+              <ul className='header__nav-list'>
+                <li className='header__nav-item user'>
+                  <Link to={AppRoute.Login} className='header__nav-link header__nav-link--profile'>
+                    <div className='header__avatar-wrapper user__avatar-wrapper'></div>
+                    <span className='header__login'>Sign in</span>
+                  </Link>
+                </li>
               </ul>
             </nav>
           }
